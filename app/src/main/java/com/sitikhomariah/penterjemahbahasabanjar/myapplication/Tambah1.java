ca696af75637e50900faebcbea91eb075283614e
siko <sitikhomariah2809@gmail.com>
2018-07-13 23:17:49 +0700

    init

--- app/src/main/java/com/sitikhomariah/penterjemahbahasabanjar/myapplication/Tambah1.java
+++ app/src/main/java/com/sitikhomariah/penterjemahbahasabanjar/myapplication/Tambah1.java
@@ -1,7 +1,10 @@
 package com.sitikhomariah.penterjemahbahasabanjar.myapplication;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sitikhomariah.penterjemahbahasabanjar.myapplication.entity.Subjek;
 
 public class Tambah1 extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Tambah1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah1);
+
+        public boolean insertSubjek(Subjek subjek) {
+            String sql = "INSERT INTO SUBJEK('Banjar','Indonesia')VALUES(?,?)";
+            Cursor cursor = database.rawQuery(sql, new String[]{subjek.getBanjar(), subjek.getIndonesia()});
+            if (cursor.moveToFirst()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
--- app/src/main/java/com/sitikhomariah/penterjemahbahasabanjar/myapplication/Tambah2.java
+++ app/src/main/java/com/sitikhomariah/penterjemahbahasabanjar/myapplication/Tambah2.java
@@ -1,7 +1,10 @@
 package com.sitikhomariah.penterjemahbahasabanjar.myapplication;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sitikhomariah.penterjemahbahasabanjar.myapplication.entity.Predikat;
 
 public class Tambah2 extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Tambah2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah2);
+
+        public boolean insertPredikat(Predikat predikat){
+            String sql = "INSERT INTO PREDIKAT('Banjar','Indonesia')VALUES(?,?)";
+            Cursor cursor = database.rawQuery(sql, new String[]{predikat.getBanjar(), predikat.getIndonesia()});
+            if (cursor.moveToFirst()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
