eb060d4618c34abda3bca829bab108c1d5bbe9ef
k.adi@greenlabs.or.id <lutvie72@gmail.com>
2018-07-14 20:59:27 +0700

    update insert

--- app/src/main/java/com/sitikhomariah/penterjemahbahasabanjar/myapplication/Tambah1.java
+++ app/src/main/java/com/sitikhomariah/penterjemahbahasabanjar/myapplication/Tambah1.java
@@ -3,6 +3,11 @@ package com.sitikhomariah.penterjemahbahasabanjar.myapplication;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sitikhomariah.penterjemahbahasabanjar.myapplication.entity.Subjek;
 
@@ -13,14 +18,28 @@ public class Tambah1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah1);
 
-        public boolean insertSubjek(Subjek subjek) {
-            String sql = "INSERT INTO SUBJEK('Banjar','Indonesia')VALUES(?,?)";
-            Cursor cursor = database.rawQuery(sql, new String[]{subjek.getBanjar(), subjek.getIndonesia()});
-            if (cursor.moveToFirst()) {
-                return true;
-            } else {
-                return false;
+        final EditText indonesia = (EditText) findViewById(R.id.editText_indonesia);
+        final EditText banjar = (EditText) findViewById(R.id.editText_banjar);
+        Button buttonSave = (Button) findViewById(R.id.button);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(indonesia.getText()) || TextUtils.isEmpty(banjar.getText())) {
+                    Toast.makeText(getApplicationContext(), "field tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Subjek subjek = new Subjek();
+                    subjek.setBanjar(banjar.getText().toString());
+                    subjek.setIndonesia(indonesia.getText().toString());
+
+                    boolean result = DatabaseAccess.getInstance(getApplicationContext()).insertSubjek(subjek);
+                    if (result) {
+                        Toast.makeText(getApplicationContext(), "Berhasil disimpan!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Gagal disimpan!", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
-        }
+        });
     }
 }
